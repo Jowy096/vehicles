3d1a7e8214ac0d6cb4e8961427dd6ecf0b7c3f16
Jowy <Jowy@LAPTOP-RMMCRSUL.home>
2018-05-05 20:51:53 +0200

    Rama master

---
+++ vehicles/src/vehicles/Vehicle.java
@@ -0,0 +1,14 @@
+package vehicles;
+
+public class Vehicle {
+	public static final int VELOCITAT_MAXIMA=120;
+	private int cilindrada;
+	private int Portes;
+	private double velocitat_actual;
+	
+	public  Vehicle(double velocitat_actual){
+		
+		this.velocitat_actual=velocitat_actual;
+	}
+
+}
